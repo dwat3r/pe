afa32f28c3091a3707be593e3d6f9e1cb8893f9f
dwat3r <hechtloliver@gmail.com>
2017-11-02 16:36:30 +0100

    working gen efficiently

--- 411/UphillPaths.hs
+++ 411/UphillPaths.hs
@@ -1,11 +1,12 @@
-
-
 import qualified Data.Set as S
 import Point
+
 -- gen n = take n $ iterate (\(bx,by) -> (bx * 2 `mod` n,by * 3 `mod` n)) (1,1)
 
-gen :: Int -> S.Set [Point]
-gen n = gen' n S.empty
-
-gen' :: Int -> S.Set [Point] -> S.Set [Point]
-gen' n s = 
+gen :: Int -> S.Set Point
+gen n = gen' n (P (1, 1)) $ S.fromList [P (0,0),P (1, 1), P (n,n)]
+  where
+    gen' :: Int -> Point -> S.Set Point -> S.Set Point
+    gen' n p s | next n p `S.member` s = s
+               | otherwise = gen' n (next n p) (next n p `S.insert` s)
+    next n (P (x, y)) = P (x * 2 `mod` n ,y * 3 `mod` n)
