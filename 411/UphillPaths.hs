979f42f5c1d30956b90c3954c16afd4778c5573c
dwat3r <hechtloliver@gmail.com>
2017-11-08 17:19:57 +0100

    initial not working concept

--- 411/UphillPaths.hs
+++ 411/UphillPaths.hs
@@ -1,9 +1,13 @@
 module UphillPaths where
 
+import Debug.Trace
+
 import qualified Data.Set as S
-import Point
 import Data.Tree
 import Data.Tree.Zipper
+
+import Point
+import Draw
 
 data Frontier = F {p :: TreePos Full Point, depth :: Int}
   deriving Eq
@@ -23,12 +27,18 @@ gen n = gen' n (P 1 1) $ S.fromList [P 1 1, P n n]
     next n (P x y) = P (x * 2 `mod` n) (y * 3 `mod` n)
 
 
-points n = foldl processPoints (root, frontStart) ps
-  where
-    ps = gen n
-    root = Node (P 0 0) []
-    frontStart = S.fromList [F (fromTree root) 0]
-    processPoints ((tree, frontier), p) = undefined--(foldl processFrontier (newElem, newBranch) frontier
-    processFrontier = undefined
+points :: Int -> [S.Set Frontier]
+points n = scanl processPoints frontStart $ S.toList $ gen n
+--where
+rootNode = Node (P 0 0) []
+frontStart = S.singleton $ F (fromTree rootNode) 0
 
---insert
+
+processPoints :: S.Set Frontier -> Point -> S.Set Frontier
+processPoints frontier p = foldl (processFrontier p) S.empty frontier
+
+
+processFrontier :: Point -> S.Set Frontier -> Frontier -> S.Set Frontier
+processFrontier p newFrontier f@(F fp d) | label fp <= p = {- traceShow  fp $ -} (insertPoint f p) `S.insert` newFrontier
+                                         | otherwise = newFrontier
+insertPoint (F fp d) p = F (insert (Node p []) (children fp)) $ d + 1
