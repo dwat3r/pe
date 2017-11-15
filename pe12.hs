ce8d47c0e4c0f12963b62f6f3789cebaec3dcf67
dwat3r <hechtloliver@gmail.com>
2017-11-15 18:10:32 +0100

    pe12 finish

--- pe12.hs
+++ pe12.hs
@@ -1,7 +1,7 @@
 module Main where
 
 import Data.List (foldl')
-
+import qualified Data.Map.Strict as M
 import Debug.Trace
 
 main :: IO ()
@@ -9,10 +9,12 @@ main = print $ go 1 0
 
 go :: Int -> Int -> Int
 go n sum | divisors (sum + n) > 500 = sum + n
-         | otherwise = traceShow (divisors (sum + n), sum + n) $ go (n+1) (sum + n)
-divisors n = foldl' (*) 1 [2..(getDivisors 0 n 2 n)]
+         | otherwise = go (n+1) (sum + n)
+
+divisors :: Int -> Int
+divisors n = getDivisors M.empty n 2 n
 
 -- amount, accum, currDivisor, original num
-getDivisors n acc d nn   | acc == 1 || d == nn `div` 2 = n
-                         | acc `mod` d == 0 = getDivisors (n+1) (acc `div` d) d nn
-                         | otherwise = getDivisors n acc (d+1) nn
+getDivisors ds acc d nn   | acc == 1 || d == nn `div` 2 = foldl' (*) 1 $ M.elems $ M.map (+1) ds
+                          | acc `mod` d == 0 = getDivisors (M.insertWith (+) d 1 ds) (acc `div` d) d nn
+                          | otherwise = getDivisors ds acc (d+1) nn
